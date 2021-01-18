a054d373c6f8d693573acf9c5132de268258bbd0
Przemyslaw Juda <przemyslaw.juda@gmail.com>
2021-01-18 15:56:41 +0100

    New submission

--- 1-prior.py
+++ 1-prior.py
@@ -9,15 +9,22 @@ from fluvial import FluvialSimulation
 from tracer import TracerTest
 
 import geone
+import time
 
 def sample_from_prior(i):
-    fluvial_simulation = FluvialSimulation(nthreads=2)
+    #fluvial_simulation = FluvialSimulation(nthreads=2)
+    fluvial_simulation = FluvialSimulation(nthreads=1)
     img_geone = fluvial_simulation.generate_with_hd(i)
-    with open('prior/img{}.pickle'.format(i), 'wb') as file_handle:
-        pickle.dump(img_geone, file_handle)
+    #with open('prior/img{}.pickle'.format(i), 'wb') as file_handle:
+    #    pickle.dump(img_geone, file_handle)
 
 def main():
-    Parallel(n_jobs=32)(delayed(sample_from_prior)(i) for i in range(1024))
+    start=time.time()
+    #Parallel(n_jobs=32)(delayed(sample_from_prior)(i) for i in range(1024))
+    Parallel(n_jobs=63)(delayed(sample_from_prior)(i) for i in range(5*63))
+    end = time.time()-start
+    with open('time.pickle', 'wb') as fh:
+        pickle.dump(end, fh)
 
 if __name__=='__main__':
     main()
